b6c9acd6b0de02350ae84678bce749eeed45aa5f
BRKN BLTC <baltaci600@gmail.com>
2022-11-29 10:01:11 +0100

    Kalendar (monat) anzeigen für das aktuelle Datum

--- GetDateInfo.cs
+++ GetDateInfo.cs
@@ -8,5 +8,32 @@ namespace Calendar
 {
     internal class GetDateInfo
     {
+        DateTime timeInfo = DateTime.Now;
+        int monthInt;
+        int yearInt;
+
+        public GetDateInfo(int monthNumber, int yearNumber)
+        {
+            monthInt = monthNumber;
+            yearInt = yearNumber;   
+        }
+        public GetDateInfo() { }
+        public int CurrentMonthNumber()
+        {
+            var month = timeInfo.Month;
+            return month;
+        }
+
+        public int CurrentDaysNumber()
+        {
+            var days = DateTime.DaysInMonth(timeInfo.Year, timeInfo.Month);
+            return days;
+        }
+
+        public int GetMonthDays()
+        {
+            var days = DateTime.DaysInMonth(yearInt, monthInt);
+            return days;
+        }
     }
 }
--- ShowCalendar.cs
+++ ShowCalendar.cs
@@ -8,21 +8,30 @@ namespace Calendar
 {
     internal class ShowCalendar
     {
+        GetDateInfo di = new GetDateInfo();
         private string userInfo;
         public ShowCalendar(string user)
         {
             userInfo = user;
             ShowCurrentUser();
+            Calendar();
         }
 
         public void ShowCurrentUser()
         {
-            Console.WriteLine(userInfo);    
+            Console.WriteLine(userInfo);   
+            Console.WriteLine("----------------------");
         }
 
         public void Calendar()
         {
-            Console.WriteLine();
+            Console.WriteLine("Month:" + di.CurrentMonthNumber());
+            Console.WriteLine("----------------------");
+            for(int i = 1; i <= di.CurrentDaysNumber(); i++)
+            {
+                Console.WriteLine(i);
+            }
+            
         }
     }
 }
