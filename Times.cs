1dff7098da1ac7604b4abfe970e5d5683a4b97af
BRKN BLTC <baltaci600@gmail.com>
2023-03-29 13:52:40 +0200

    added times

---
+++ Times.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calendar
+{
+    internal class Times 
+    {
+        private Hashtable table;
+        private ArrayList times = new ArrayList();
+
+        Times()
+        {
+            setTimes();
+        }
+        public void showTime()
+        {
+            foreach (var t in times)
+            {
+                Console.WriteLine(t.ToString());
+            }
+        }
+        
+        public void setTimes()
+        {
+            for (int i = 0; i < 24; i++)
+            {
+                this.times.Add(i + "o'clock");
+            }                
+        }
+    }
+}
---
+++ enums/TimeNumbers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calendar.enums
+{
+    internal enum TimeNumbers   
+    {
+        AM1, 
+        AM2,         
+        AM3, 
+        AM4, 
+        AM5, 
+        AM6, 
+        AM7, 
+        AM8, 
+        AM9, 
+        AM10, 
+        AM11, 
+        AM12, 
+        PM1, 
+        PM2, 
+        PM3, 
+        PM4, 
+        PM5, 
+        PM6, 
+        PM7, 
+        PM8, 
+        PM9, 
+        PM10, 
+        PM11, 
+        PM12
+    }
+}
