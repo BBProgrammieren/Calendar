b378f8b133a97823427a527d378a2e22e8709e98
BRKN BLTC <baltaci600@gmail.com>
2023-05-31 10:18:12 +0200

    Update select user from list

--- Role.cs
+++ Role.cs
@@ -145,6 +145,7 @@ internal class Role
     {
         bool pressedEnter = false;  
         int selection = 0;
+        UserManager uM = null;
 
         showUserList(selection);       
 
@@ -173,9 +174,8 @@ internal class Role
                 }
             }
             else if (cki.Key == ConsoleKey.Enter)
-            {
-                pressedEnter = true;
-                return userManager = new UserManager((string)allUsers[selection]);
+            {          
+                return new UserManager((string)allUsers[selection]);
             }
             else if (cki.Key == ConsoleKey.D)
             {
@@ -185,9 +185,259 @@ internal class Role
             {
                 return null;
             }
+            else if (cki.Key == ConsoleKey.W)
+            {
+                Console.Clear();
+                Console.WriteLine("The key 'W' have no function!");
+                Console.WriteLine("Please read the info!");
+                Console.WriteLine("Press enter to continue!");
+                Console.ReadKey();
+                Console.Clear();
+                return selectUserfromList();
+            }
+            else if (cki.Key == ConsoleKey.E)
+            {
+                Console.Clear();
+                Console.WriteLine("The key 'E' have no function!");
+                Console.WriteLine("Please read the info!");
+                Console.WriteLine("Press enter to continue!");
+                Console.ReadKey();
+                Console.Clear();
+                return selectUserfromList();
+            }
+            else if (cki.Key == ConsoleKey.R)
+            {
+                Console.Clear();
+                Console.WriteLine("The key 'R' have no function!");
+                Console.WriteLine("Please read the info!");
+                Console.WriteLine("Press enter to continue!");
+                Console.ReadKey();
+                Console.Clear();
+                return selectUserfromList();
+            }
+            else if (cki.Key == ConsoleKey.T)
+            {
+                Console.Clear();
+                Console.WriteLine("This key have no function!");
+                Console.WriteLine("Please read the info!");
+                Console.WriteLine("Press enter to continue!");
+                Console.ReadKey();
+                Console.Clear();
+                return selectUserfromList();
+            }
+            else if (cki.Key == ConsoleKey.Z)
+            {
+                Console.Clear();
+                Console.WriteLine("This key have no function!");
+                Console.WriteLine("Please read the info!");
+                Console.WriteLine("Press enter to continue!");
+                Console.ReadKey();
+                Console.Clear();
+                return selectUserfromList();
+            }
+            else if (cki.Key == ConsoleKey.U)
+            {
+                Console.Clear();
+                Console.WriteLine("This key have no function!");
+                Console.WriteLine("Please read the info!");
+                Console.WriteLine("Press enter to continue!");
+                Console.ReadKey();
+                Console.Clear();
+                return selectUserfromList();
+            }
+            else if (cki.Key == ConsoleKey.I)
+            {
+                Console.Clear();
+                Console.WriteLine("This key have no function!");
+                Console.WriteLine("Please read the info!");
+                Console.WriteLine("Press enter to continue!");
+                Console.ReadKey();
+                Console.Clear();
+                return selectUserfromList();
+            }
+            else if (cki.Key == ConsoleKey.O)
+            {
+                Console.Clear();
+                Console.WriteLine("This key have no function!");
+                Console.WriteLine("Please read the info!");
+                Console.WriteLine("Press enter to continue!");
+                Console.ReadKey();
+                Console.Clear();
+                return selectUserfromList();
+            }
+            else if (cki.Key == ConsoleKey.P)
+            {
+                Console.Clear();
+                Console.WriteLine("This key have no function!");
+                Console.WriteLine("Please read the info!");
+                Console.WriteLine("Press enter to continue!");
+                Console.ReadKey();
+                Console.Clear();
+                return selectUserfromList();
+            }
+            else if (cki.Key == ConsoleKey.A)
+            {
+                Console.Clear();
+                Console.WriteLine("This key have no function!");
+                Console.WriteLine("Please read the info!");
+                Console.WriteLine("Press enter to continue!");
+                Console.ReadKey();
+                Console.Clear();
+                return selectUserfromList();
+            }
+            else if (cki.Key == ConsoleKey.S)
+            {
+                Console.Clear();
+                Console.WriteLine("This key have no function!");
+                Console.WriteLine("Please read the info!");
+                Console.WriteLine("Press enter to continue!");
+                Console.ReadKey();
+                Console.Clear();
+                return selectUserfromList();
+            }
+            else if (cki.Key == ConsoleKey.F)
+            {
+                Console.Clear();
+                Console.WriteLine("This key have no function!");
+                Console.WriteLine("Please read the info!");
+                Console.WriteLine("Press enter to continue!");
+                Console.ReadKey();
+                Console.Clear();
+                return selectUserfromList();
+            }
+            else if (cki.Key == ConsoleKey.G)
+            {
+                Console.Clear();
+                Console.WriteLine("The key 'G' have no function!");
+                Console.WriteLine("Please read the info!");
+                Console.WriteLine("Press enter to continue!");
+                Console.ReadKey();
+                Console.Clear();
+                return selectUserfromList();
+            }
+            else if (cki.Key == ConsoleKey.H)
+            {
+                Console.Clear();
+                Console.WriteLine("The key 'H' have no function!");
+                Console.WriteLine("Please read the info!");
+                Console.WriteLine("Press enter to continue!");
+                Console.ReadKey();
+                Console.Clear();
+                return selectUserfromList();
+            }
+            else if (cki.Key == ConsoleKey.J)
+            {
+                Console.Clear();
+                Console.WriteLine("The key 'J' have no function!");
+                Console.WriteLine("Please read the info!");
+                Console.WriteLine("Press enter to continue!");
+                Console.ReadKey();
+                Console.Clear();
+                return selectUserfromList();
+            }
+            else if (cki.Key == ConsoleKey.K)
+            {
+                Console.Clear();
+                Console.WriteLine("The key 'K' have no function!");
+                Console.WriteLine("Please read the info!");
+                Console.WriteLine("Press enter to continue!");
+                Console.ReadKey();
+                Console.Clear();
+                return selectUserfromList();
+            }
+            else if (cki.Key == ConsoleKey.L)
+            {
+                Console.Clear();
+                Console.WriteLine("The key 'L' have no function!");
+                Console.WriteLine("Please read the info!");
+                Console.WriteLine("Press enter to continue!");
+                Console.ReadKey();
+                Console.Clear();
+                return selectUserfromList();
+            }
+            else if (cki.Key == ConsoleKey.Y)
+            {
+                Console.Clear();
+                Console.WriteLine("The key 'Y' have no function!");
+                Console.WriteLine("Please read the info!");
+                Console.WriteLine("Press enter to continue!");
+                Console.ReadKey();
+                Console.Clear();
+                return selectUserfromList();
+            }
+            else if (cki.Key == ConsoleKey.X)
+            {
+                Console.Clear();
+                Console.WriteLine("The key 'X' have no function!");
+                Console.WriteLine("Please read the info!");
+                Console.WriteLine("Press enter to continue!");
+                Console.ReadKey();
+                Console.Clear();
+                return selectUserfromList();
+            }
+            else if (cki.Key == ConsoleKey.C)
+            {
+                Console.Clear();
+                Console.WriteLine("The key 'C' have no function!");
+                Console.WriteLine("Please read the info!");
+                Console.WriteLine("Press enter to continue!");
+                Console.ReadKey();
+                Console.Clear();
+                return selectUserfromList();
+            }
+            else if (cki.Key == ConsoleKey.V)
+            {
+                Console.Clear();
+                Console.WriteLine("The key 'V' have no function!");
+                Console.WriteLine("Please read the info!");
+                Console.WriteLine("Press enter to continue!");
+                Console.ReadKey();
+                Console.Clear();
+                return selectUserfromList();
+            }
+            else if (cki.Key == ConsoleKey.B)
+            {
+                Console.Clear();
+                Console.WriteLine("The key 'B' have no function!");
+                Console.WriteLine("Please read the info!");
+                Console.WriteLine("Press enter to continue!");
+                Console.ReadKey();
+                Console.Clear();
+                return selectUserfromList();
+            }
+            else if (cki.Key == ConsoleKey.N)
+            {
+                Console.Clear();
+                Console.WriteLine("The key 'N' have no function!");
+                Console.WriteLine("Please read the info!");
+                Console.WriteLine("Press enter to continue!");
+                Console.ReadKey();
+                Console.Clear();
+                return selectUserfromList();
+            }
+            else if (cki.Key == ConsoleKey.M)
+            {
+                Console.Clear();
+                Console.WriteLine("The key 'M' have no function!");
+                Console.WriteLine("Please read the info!");
+                Console.WriteLine("Press enter to continue!");
+                Console.ReadKey();
+                Console.Clear();
+                return selectUserfromList();
+            }
+            else if (cki.Key == ConsoleKey.Spacebar)
+            {
+                Console.Clear();
+                Console.WriteLine("The spacebar have no function!");
+                Console.WriteLine("Please read the info!");
+                Console.WriteLine("Press enter to continue!");
+                Console.ReadKey();
+                Console.Clear();
+                return selectUserfromList();
+            }
         } while (pressedEnter = true);
 
-        return null;
+        return uM;
     }
 
     public void deleteUser(int selection)
